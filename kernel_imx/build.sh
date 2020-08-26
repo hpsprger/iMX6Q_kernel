6bbb99c5d20940a8b328428de7d3514d7b98dd9d
hpsprger <hpsprger@gmail.com>
2020-08-27 07:43:39 +0800

    mdf

--- kernel_imx/build.sh
+++ kernel_imx/build.sh
@@ -2,10 +2,16 @@
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabihf-
 cp config_linux .config
+
+echo make dtbs .......
 make dtbs
 
-make zImage -j8
+#make zImage -j8
+echo make zImage ......
+make zImage
+echo  make modules ......
 make modules
+echo all done ...... 
 cp arch/arm/boot/dts/e9v2-sabresd.dtb ./
 cp arch/arm/boot/dts/e9v3-sabresd.dtb ./
 cp arch/arm/boot/dts/imx6q-sabresd.dtb ./

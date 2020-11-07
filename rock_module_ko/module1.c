c1add838249e9ac954a21253ea136b753ed1438f
hpsprger <hpsprger@gmail.com>
2020-11-07 09:59:28 +0800

    mdf

--- rock_module_ko/module1.c
+++ rock_module_ko/module1.c
@@ -1,4 +1,22 @@
-#include<stdio.h>
+#include <linux/init.h>
+#include <linux/module.h>
+ 
+MODULE_LICENSE("Dual BSD/GPL");
 
-xxx
+static int hello_init(void)
+{
+    printk(KERN_EMERG "Hello world enter \n");
+    return 0;
+}
 
+static void hello_exit(void)
+{
+    printk(KERN_EMERG "Hello world exit \n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_AUTHOR("Song Baohua");
+MODULE_DESCRIPTION("A simple Hello world module");
+MODULE_ALIAS("a simplest module");

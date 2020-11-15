8818cd717794d3c048c93b646c2e9b751d23a06b
hpsprger <hpsprger@gmail.com>
2020-11-15 23:07:23 +0800

    mdf

--- rock_module_ko/rock_net_card.c
+++ rock_module_ko/rock_net_card.c
@@ -1,15 +1,24 @@
 #include <linux/init.h>
 #include <linux/module.h>
  
+extern void hello_fun(void);
 
-static int hello2_init(void)
+static int rocke_net_card_init(void)
 {
-    printk(KERN_EMERG "Hello2 world enter \n");
+    hello_fun();
+    printk(KERN_EMERG "rock net card init....\n");
     return 0;
 }
 
-static void hello2_exit(void)
+static void rock_net_card_exit(void)
 {
-    printk(KERN_EMERG "Hello2 world exit \n");
+    printk(KERN_EMERG "rock net card exit....\n");
 }
+
+
+module_init(rock_module_init);
+module_exit(rock_module_exit);
+
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_AUTHOR("ROCKLEE");
 
